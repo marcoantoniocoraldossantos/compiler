4ab210e5b9016999b9bdefaf95662daf944d214f
marcoantoniocoraldossantos <marcoantoniocoraldossantos@gmail.com>
2023-10-16 15:57:37 -0300

    added lexer functions

---
+++ lexer.c
@@ -0,0 +1,10 @@
+// libraries used
+#include <stdio.h>
+#include <stdlib.h>
+
+// include all header files
+#include "utilities.h"
+#include "buffer.h"
+#include "token.h"
+#include "lexer.h"
+#include "error.h"
