30b21bbc38001531b02e541a4336e1d9a7670657
marcoantoniocoraldossantos <marcoantoniocoraldossantos@gmail.com>
2023-10-16 15:55:43 -0300

    added main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+// libraries used
+#include <stdio.h>
+#include <stdlib.h>
+
+// include all header files
+#include "utilities.h"
+#include "buffer.h"
+#include "token.h"
+#include "lexer.h"
+#include "error.h"
+
+int main(int argc, char *argv[]) 
+{
+    verify_arguments(argc, argv);
+
+    FILE *input_file = open_file(argv[1], "r");
+
+    token_list_t *token_list = lexical_analyzer(input_file);
+
+    free_token_list(token_list);
+    close_file(input_file);
+
+    return 0;
+}
