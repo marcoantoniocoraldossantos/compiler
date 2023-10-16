c8372f39b2d3babe4469012a2cc82ca8e9e23d56
marcoantoniocoraldossantos <marcoantoniocoraldossantos@gmail.com>
2023-10-16 16:01:53 -0300

    update lexer.h

--- lexer.h
+++ lexer.h
@@ -1,10 +1,37 @@
-// libraries used
-#include <stdio.h>
-#include <stdlib.h>
+// lexer.h
+#ifndef LEXER_H
+#define LEXER_H
 
-// include all header files
-#include "utilities.h"
-#include "buffer.h"
+#include <stdio.h>
 #include "token.h"
-#include "lexer.h"
-#include "error.h"
+
+token_list_t* lexical_analyzer(FILE *source_code_file);
+
+#endif
+
+
+
+/*
+typedef struct {
+    int transitionTable[NUM_STATES][NUM_CHAR_CLASSES];
+    int acceptanceTable[NUM_STATES];
+    // ... other necessary structures and types
+} LexerDFA;
+
+
+typedef enum {
+    START_STATE,
+    // Add more states as needed for your DFA
+    STATE1,
+    STATE2,
+    // ...
+    FINAL_STATE,
+    ERROR_STATE
+} DFState;
+
+
+void initializeLexerDFA(LexerDFA *dfa);
+int getNextState(LexerDFA *dfa, int currentState, int charClass);
+int isCharUsed(int charClass);
+int isAcceptanceState(LexerDFA *dfa, int state);
+*/
