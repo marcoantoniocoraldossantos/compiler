c270c990ea7d82cbae8048b6986940f307f27359
marcoantoniocoraldossantos <marcoantoniocoraldossantos@gmail.com>
2023-10-16 16:04:08 -0300

    update buffer.h

---
+++ buffer.h
@@ -0,0 +1,19 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+
+
+typedef struct 
+{
+    char *data;
+    int size;
+    int line;
+    int position;
+} buffer_t;
+
+buffer_t allocate_buffer(int size);
+void deallocate_buffer(buffer_t *buffer);
+void fill_buffer(FILE *file, buffer_t *buffer);
+void print_buffer(buffer_t *buffer);
+
+#endif
