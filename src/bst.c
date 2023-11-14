5b49534121ca5e83bf0b735025926b95a618194e
marcoantoniocoraldossantos <marcoantoniocoraldossantos@gmail.com>
2023-11-14 14:14:35 -0300

    update bst.c

--- src/bst.c
+++ src/bst.c
@@ -1,21 +1,23 @@
 #include "libraries.h"
 
+// all the reserved words
 char* reserved_words[] = 
 {
     "else", "if", "int", "return", "void", "while"
 };
 
+// function to find the optimal reserved word to be the root of the bst
 char *find_optimal_root()
 {
     if (reserved_words == NULL || reserved_words[0] == NULL) 
     {
-        return NULL; // Handle empty list
+        return NULL; //if there are no reserved words
     }
 
     int num_words = 0;
     int sum_ascii = 0;
 
-    // Calculate the average ASCII value of the first letter
+    // find the average ascii value of the first letter of each word
     for (int i = 0; reserved_words[i] != NULL; i++) 
     {
         if (isalpha(reserved_words[i][0])) 
@@ -31,56 +33,52 @@ char *find_optimal_root()
     }
 
     int average_ascii = sum_ascii / num_words;
-    int min_difference = 255;
+    int min_difference = 255; // max ascii value
 
     char* optimal_root = NULL;
 
+    // find the word with the smallest difference between its first letter's ascii value and the average ascii value
     for (int i = 0; reserved_words[i] != NULL; i++) 
     {
         if (isalpha(reserved_words[i][0])) 
         {
             int word_ascii = (int)(unsigned char)reserved_words[i][0];
-            int difference = abs(average_ascii - word_ascii);
+            int difference = abs(average_ascii - word_ascii); // absolute value of the difference between the average ascii value and the word's first letter's ascii value
 
             if (difference < min_difference) 
             {
                 min_difference = difference;
-                optimal_root = reserved_words[i];
+                optimal_root = reserved_words[i]; // the word with the smallest difference is the optimal root
             }
         }
     }
 
-    return optimal_root;
+    return optimal_root; // return the optimal root  
 }
 
+// function toinitialize the bst
 bst_node_t* initialize_bst() 
 {
     bst_node_t* root = NULL;
 
-    char *optimal_root = find_optimal_root();
+    char *optimal_root = find_optimal_root(); // find the optimal root
     //printf("best root is %s\n", optimal_root);
 
-    root = insert_bst_node(root, optimal_root, reserved_word_token_type(optimal_root));
+    root = insert_bst_node(root, optimal_root, reserved_word_token_type(optimal_root)); // insert the optimal word as the root
     
     //insert rest of the reserved words
     for(int i = 0; reserved_words[i] != NULL; i++)
     {
-        if(strcmp(reserved_words[i], optimal_root) != 0)
+        if(strcmp(reserved_words[i], optimal_root) != 0) // if the word is not the optimal root
         {
-            root = insert_bst_node(root, reserved_words[i], reserved_word_token_type(reserved_words[i]));
+            root = insert_bst_node(root, reserved_words[i], reserved_word_token_type(reserved_words[i])); // insert the word
         }
     }
-
-    // root = insert_bst_node(root, "return", RETURN);
-    // root = insert_bst_node(root, "void", VOID);
-    // root = insert_bst_node(root, "int", INT);
-    // root = insert_bst_node(root, "if", IF);
-    // root = insert_bst_node(root, "else", ELSE);
-    // root = insert_bst_node(root, "while", WHILE);
 
     return root;
 }
 
+// function to print the bst
 void print_bst(bst_node_t* root, int level) 
 {
     if (root == NULL) 
@@ -99,6 +97,7 @@ void print_bst(bst_node_t* root, int level)
     print_bst(root->left, level + 1);
 }
 
+// function to insert a node in the bst
 bst_node_t* insert_bst_node(bst_node_t* root, char* lexeme, token_type_t token_type) 
 {
     if (root == NULL) 
@@ -131,6 +130,7 @@ bst_node_t* insert_bst_node(bst_node_t* root, char* lexeme, token_type_t token_t
     return root;
 }
 
+// function to free the bst
 void free_bst(bst_node_t* root) 
 {
     if (root == NULL) 
