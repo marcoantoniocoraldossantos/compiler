61d4d2ed28d24f0c30d0663a5d31689d26b2141d
marcoantoniocoraldossantos <marcoantoniocoraldossantos@gmail.com>
2023-11-19 14:57:37 -0300

    update tree node

--- include/tree.h
+++ include/tree.h
@@ -1,6 +1,11 @@
 #ifndef TREE_H
 #define TREE_H
 
+typedef enum {
+    STATEMENT_NODE,
+    EXPRESSION_NODE
+} node_type_t;
+
 typedef enum 
 {
     ASSIGN_STMT,        // id = expression;
@@ -19,18 +24,20 @@ typedef enum
 } expression_kind_t;
 
 // Define your tree node structure
-typedef struct parse_tree_node_t 
-{
-    // Data in the node
-    int data; // Replace 'int' with your desired data type
-    struct parse_tree_node_t *child; // Pointer to the first child node
-    struct parse_tree_node_t *sibling; // Pointer to the next sibling node
+typedef struct parse_tree_node_t {
+    int line; // Linha do código-fonte
+    char lexeme[64]; // Lexema (com tamanho máximo de 64 caracteres)
+    node_type_t node_type; // Indica se é um nó de declaração ou expressão
+    statement_type_t statement_type; // Tipo de declaração
+    expression_type_t expression_type; // Tipo de expressão
+    struct parse_tree_node_t *child; // Ponteiro para o primeiro nó filho
+    struct parse_tree_node_t *sibling; // Ponteiro para o próximo nó irmão
 } parse_tree_node_t;
 
 // Function prototypes
-TreeNode* create_node(int data);
-void add_child(TreeNode *parent, TreeNode *child);
-void add_sibling(TreeNode *node, TreeNode *sibling);
-void delete_tree(TreeNode *root);
+parse_tree_node_t* create_node(int line, char *lexeme, node_type_t node_type, statement_type_t statement_type, expression_type_t expression_type));
+void add_child(parse_tree_node_t *parent, parse_tree_node_t *child);
+void add_sibling(parse_tree_node_t *node, parse_tree_node_t *sibling);
+void delete_tree(parse_tree_node_t *root);
 
 #endif /* TREE_H */
