a6a1fb2ee5113cadb189340e3a8ca921238e6a48
marcoantoniocoraldossantos <marcoantoniocoraldossantos@gmail.com>
2023-11-19 14:12:20 -0300

    added enums

--- include/tree.h
+++ include/tree.h
@@ -1,6 +1,24 @@
 #ifndef TREE_H
 #define TREE_H
 
+typedef enum {
+    ASSIGNMENT_STMT,  // Atribuição (ex: x = 5;)
+    IF_STMT,          // Declaração IF (ex: if (condição) { ... } [else { ... }])
+    WHILE_STMT,       // Declaração WHILE (ex: while (condição) { ... })
+    RETURN_STMT,      // Declaração RETURN (ex: return x;)
+    // Outros tipos de declarações, se aplicáveis...
+} StatementKind;
+
+typedef enum {
+    OPERATION_EXP,    // Expressão de operação (ex: x + y)
+    CONSTANT_EXP,     // Expressão de constante (ex: 10, 'a', "string")
+    VARIABLE_EXP,     // Expressão de variável (ex: x, y)
+    RELATIONAL_EXP,   // Expressão relacional (ex: x > y, x <= y)
+    EQUALITY_EXP,     // Expressão de igualdade (ex: x == y, x != y)
+    // Outros tipos de expressões, se aplicáveis...
+} ExpressionKind;
+
+
 // Define your tree node structure
 typedef struct TreeNode {
     // Data in the node
