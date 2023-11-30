bdb3c7330459904362fc016e762edc67e5eae6d1
marcoantoniocoraldossantos <marcoantoniocoraldossantos@gmail.com>
2023-11-30 10:30:18 -0300

    added basic functions of symtab

---
+++ include/symtab.h
@@ -0,0 +1,44 @@
+#ifndef SYMTAB_H
+#define SYMTAB_H
+
+#define MAX_LEXEME_LENGHT 64
+
+typedef enum 
+{
+    INT_TYPE,
+    VOID_TYPE,
+} data_type_t;
+
+typedef enum 
+{
+    VARIABLE,
+    FUNCTION,
+} id_type_t;
+
+typedef enum 
+{
+    GLOBAL,
+    LOCAL,
+} scope_t;
+
+typedef struct symtab_entry_t 
+{
+    char name[MAX_LEXEME_LENGHT];
+    data_type_t data_type;
+    id_type_t id_type;
+    int line_number;
+    char scope[MAX_LEXEME_LENGHT];
+    scope_t scope_type;
+    struct symtab_entry_t* next; // Para encadeamento em caso de colisões (caso use tabela hash)
+} symtab_entry_t;
+
+// Função para inicializar a tabela de símbolos
+void initialize_symtab();
+
+// Função para inserir um símbolo na tabela de símbolos
+void insert_symbol(const char* name, data_type_t data_type, id_type_t id_type);
+
+// Função para imprimir a tabela de símbolos
+void print_symtab();
+
+#endif /* SYMTAB_H */
