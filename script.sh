f873b05f6decf88d4f6ae9771c8464d36cc42482
Marco Antonio Coral dos Santos <110304504+marcoantoniocoraldossantos@users.noreply.github.com>
2023-12-03 18:43:13 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,11 +8,9 @@ INITIAL_DIR=$(pwd)
 # Entrando na pasta bison
 cd bison || exit
 
-# Executando o Bison
 #echo "executing bison..."
 bison -d -o parser.c parser.y
 
-# Movendo os arquivos gerados para pasta src e include
 #echo "moving parser.c and parser.h..."
 mv parser.c ../src/
 mv parser.h ../include/
@@ -28,11 +26,6 @@ cd "$INITIAL_DIR" || exit
 make -s
 
 #echo "compiled successfully!"
-
-#valgrind --track-origins=yes --leak-check=full --show-leak-kinds=all
-#valgrind --track-origins=yes --leak-check=full --show-leak-kinds=all ./compiler test/example1.txt
-#valgrind --track-origins=yes --leak-check=full --show-leak-kinds=all ./compiler test/example2.txt
-#valgrind --track-origins=yes --leak-check=full --show-leak-kinds=all 
 
 #echo "testing with lexical error..."
 ./compiler test-lexical/example1.txt
